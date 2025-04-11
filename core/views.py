be202ce46c733af3b1ed53a744a12d64fe617d61
Diego Ayala <dieguito9677@gmail.com>
2025-04-11 15:12:37 -0400

    solve bug

--- core/views.py
+++ core/views.py
@@ -6,6 +6,11 @@ from .models import RadioFM
 class IndexTemplateView(TemplateView):
     template_name = 'index.html'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['total_radios'] = RadioFM.objects.count()
+        return context
+
 
 class RadioFMListView(ListView):
     model = RadioFM
